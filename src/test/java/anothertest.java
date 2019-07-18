9115a5e0814db521eba0407363ff4d7ba3e38a6f
Aman Wolde <aman.wolde@mavenwave.com>
2019-07-18 15:43:32 -0500

    error

--- src/test/java/anothertest.java
+++ src/test/java/anothertest.java
@@ -3,6 +3,6 @@ import static io.restassured.RestAssured.given;
 
 public class anothertest {
   public static void main (String[] args) {
-		 given().when().get("http://www.google.com").then().statusCode(200);
+		 given().when().get("http://www.google.com").then().statusCode(300);
   }
 }
