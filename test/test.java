d94de7f0ad35a61167c2c0c2cf0a94ae235b1e65
Aman Wolde <aman.wolde@mavenwave.com>
2019-07-18 15:09:04 -0500

    new test

---
+++ test/test.java
@@ -0,0 +1,13 @@
+import io.restassured.RestAssured;
+import io.restassured.itest.java.support.WithJetty;
+import static io.restassured.RestAssured.when;
+import static io.restassured.config.JsonConfig.jsonConfig;
+import static io.restassured.path.json.config.JsonPathConfig.NumberReturnType.DOUBLE;
+//import static org.hamcrest.Matchers.*;
+
+public class test {
+  @Test
+  public void ApiTest() {
+    System.out.print("hi there");
+  }
+}
