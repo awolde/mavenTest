97063939ca2adf596956bda7912142d669dd7269
Aman Wolde <aman.wolde@mavenwave.com>
2019-07-18 20:19:17 -0500

    Adding real working one

---
+++ src/test/java/RestTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class RestTest extends test{
+
+   public String url="http://172.18.0.1:8090";
+
+   @Test
+	 public void makeSureServerIsUp() {
+		 given().when().get(url).then().statusCode(200);
+	 }
+
+   @Test
+	 public void checkFor40x() {
+		 given().when().get(url+"/stream").then().statusCode(404);
+	 }
+
+	@Test
+	public void basicPingTest() {
+		given().when().get(url+"/stream/1").then()
+      .body("headers.User-Agent",equalTo("Apache-HttpClient/4.5.3 (Java/1.8.0_212)"))
+      .statusCode(200);
+	}
+}
--- src/test/java/anothertest.java
+++
@@ -1,8 +0,0 @@
-import io.restassured.RestAssured;
-import static io.restassured.RestAssured.given;
-
-public class anothertest {
-  public static void main (String[] args) {
-		 given().when().get("http://www.google.com").then().statusCode(300);
-  }
-}
