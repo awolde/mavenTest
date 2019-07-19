b8583daac1903f5676d06bef18086968002ae998
Aman Wolde <aman.wolde@mavenwave.com>
2019-07-18 20:01:33 -0500

    working tests

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,12 +1,34 @@
+//import static com.jayway.restassured.RestAssured.given;
 import io.restassured.RestAssured;
-import static io.restassured.RestAssured.when;
-import static io.restassured.config.JsonConfig.jsonConfig;
-import static io.restassured.path.json.config.JsonPathConfig.NumberReturnType.DOUBLE;
+//import static io.restassured.RestAssured.given;
+//import static io.restassured.config.JsonConfig.jsonConfig;
+//import static io.restassured.path.json.config.JsonPathConfig.NumberReturnType.DOUBLE;
+import org.junit.Test;
 //import static org.hamcrest.Matchers.*;
 
 public class test {
 //  @Test
-  public void ApiTest() {
-    System.out.print("hi there");
+//  @BeforeClass
+  public void setup() {
+    String port = System.getProperty("server.port");
+        if (port == null) {
+            RestAssured.port = Integer.valueOf(8090);
+        }
+        else{
+            RestAssured.port = Integer.valueOf(port);
+        }
+
+
+        String basePath = System.getProperty("server.base");
+        if(basePath==null){
+            basePath = "/stream";
+        }
+        RestAssured.basePath = basePath;
+
+        String baseHost = System.getProperty("server.host");
+        if(baseHost==null){
+            baseHost = "http://172.18.0.1";
+        }
+        RestAssured.baseURI = baseHost;
   }
 }
